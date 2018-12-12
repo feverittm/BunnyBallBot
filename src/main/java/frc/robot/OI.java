d5d73896a7ecbce31f3defa01feb19b282336738
Floyd Moore <floyd.moore@gmail.com>
2018-12-12 14:44:59 -0800

    Add in preferences to OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Preferences;
 import frc.robot.commands.AutoDriveForward;
 import frc.robot.commands.ShiftCommand;
 import frc.robot.commands.ToggleBallMode;
@@ -25,7 +26,9 @@ import frc.robot.commands.ToggleSorter;
 public class OI {
 	public Joystick 
 	GamePad1, 
-  	GamePad2;
+	  GamePad2;
+	  
+	private Preferences pref;
   
   public final JoystickButton
 	//decellToggleButton,
@@ -37,21 +40,25 @@ public class OI {
 		GamePad1 = new Joystick(RobotMap.Ports.GamePad1);
     	GamePad2 = new Joystick(RobotMap.Ports.GamePad2);
     
-    //DRIVETRAIN BUTTONS
+    	//DRIVETRAIN BUTTONS
 		shiftButton = new JoystickButton(GamePad1, RobotMap.Buttons.shiftButton);
 		shiftButton.whenPressed(new ShiftCommand());
 
-	// Control Buttons
+		// Control Buttons
 		ballModeButton = new JoystickButton(GamePad1, RobotMap.Buttons.ballmodeButton);
 		ballModeButton.whenPressed(new ToggleBallMode());
 
-	//SmartDashboard Buttons
+		//SmartDashboard Buttons
 		SmartDashboard.putData("Toggle Gather", new ToggleGatherer());
 		SmartDashboard.putData("Toggle Front Hopper", new ToggleFrontHopper());
 		SmartDashboard.putData("Toggle Sorter", new ToggleSorter());
 		SmartDashboard.putData("Toggle Rear Hopper", new ToggleRearHopper());
 		SmartDashboard.putData("Drive Forward by 500 Tics", new AutoDriveForward(500));
-  }
+
+		// Prefernces
+		pref = Preferences.getInstance();
+		rereadPreferences();
+  	}
 
 	public double getLeftY() {
 		return joystickDeadband(-GamePad1.getRawAxis(1));
@@ -70,6 +77,22 @@ public class OI {
 			return 0;
 		} else {
 			return x;
+		}
+	}
+	
+	//we tune our pid by using the robot preferences, this method updates those values
+	public void rereadPreferences() {
+		RobotMap.Values.driveDistanceP = pref.getDouble("DriveDistance P", Double.POSITIVE_INFINITY);
+		RobotMap.Values.driveDistanceI = pref.getDouble("DriveDistance I", Double.POSITIVE_INFINITY);
+		RobotMap.Values.driveDistanceD = pref.getDouble("DriveDistance D", Double.POSITIVE_INFINITY);
+		// if these are not provided
+		if (RobotMap.Values.driveDistanceP == Double.POSITIVE_INFINITY) {
+			pref.putDouble("DriveDistance P", 0);
+			pref.putDouble("DriveDistance I", 0);
+			pref.putDouble("DriveDistance D", 0);
+			RobotMap.Values.driveDistanceP = 0;
+			RobotMap.Values.driveDistanceI= 0;
+			RobotMap.Values.driveDistanceD = 0;
 		}
 	}
 	
